60cd653e9710fef193d318332674863ee243aefc
tiendungcln <tiendung0301cln@gmail.com>
2026-05-26 17:45:42 +0700

    update Level_1

--- src/Level_1.java
+++ src/Level_1.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Level_1 {
     // Selection Sort tăng dần
+    // Selection Sort giảm dần
 
     public static void selectionSortAscending(int[] arr){
 
@@ -29,10 +30,39 @@ public class Level_1 {
 
     }
 
+    public static void selectionSortDescending(int[] arr){
+
+        for (int i = 0; i < arr.length - 1; i++){
+
+            int maxIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++){
+
+                if (arr[j] > arr[maxIndex]){
+
+                    maxIndex = j;
+
+                }
+
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
     public static void main(String[] args){
 
-        int[] arr = {4, 2, 6, 8, 1};
-        selectionSortAscending(arr);
+        int[] arr1 = {4, 2, 6, 8, 1};
+        int[] arr2 = {4, 2, 6, 8, 1};
+
+        selectionSortAscending(arr1);
+        selectionSortDescending(arr2);
 
     }
 }
---
+++ src/Level_1_1.java
@@ -0,0 +1,17 @@
+public class Level_1_1 {
+    // In min sau mỗi vòng
+    // Ví dụ:
+    // Min hiện tại là: ...
+
+    public static void selectionSortAscending(int[] arr){
+
+
+
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+}
